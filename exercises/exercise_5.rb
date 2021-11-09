7331be460f189a1e80e2fa13542b1f3bdb5d8cdd
anju-kumaran <anju.naduth@gmail.com>
2021-11-09 11:50:01 -0500

    Calculations, finding sum and average

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,11 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+sum = Store.sum('annual_revenue')
+puts sum
+
+avg = Store.average('annual_revenue')
+puts avg
+
+num = Store.where("annual_revenue > 1000000").count
+puts num
